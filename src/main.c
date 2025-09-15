5b79124059e352f833f1188c4e114ba0444a9fc2
Adrien GORDON <adrien.gordon@epita.fr>
2025-09-15 14:46:24 +0200

    feat: added square function

--- src/main.c
+++ src/main.c
@@ -6,29 +6,46 @@
 int main(int argc, char *argv[]){
 	if (argc == 4)
 	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
+		char* op = argv[1];
+		char* a = argv[2];
+		char* b = argv[3];
+		double r = 0;
+		if (strcmp(op, "add") == 0) {
+			r = _add(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "sub") == 0) {
+			r = _sub(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "mul") == 0) {
+			r = _mul(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "div") == 0) {
+			r = _div(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else {printf("Erreur de parametres");}
 	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
+	else if (argc != 3) {printf("Erreur de parametres"); return -1;}
+	else {
+		char* op = argv[1];
+		char* nb = argv[2];
+
+		if (strcmp(op, "car") == -1) {
+			printf("Invalid operator");
+			return 2;
+		}
+
+		square = _car(nb);
+		if (square < 0) {
+			printf("Error: received a negative number from car(), expected a positive value");
+			return -1;
+		}
+
+		printf("Square of %d is %d", nb, square);
 	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
 	return 0;
 }
 
